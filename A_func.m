131e5b2632e2de7812f1a2d7a988a70a8d9fca9d
NeverSmile17 <62741476+NeverSmile17@users.noreply.github.com>
2020-03-27 14:42:40 +0300

    Add files via upload

---
+++ A_func.m
@@ -0,0 +1,9 @@
+function A = A_func(codes)
+size_A = length(codes(1, :)) + 1;
+A = zeros(1, size_A);
+
+w = sum(codes, 2);
+
+for i = 1 : size_A
+    A(1, i) = sum(((i - 1) == w));    слов определенного веса 
+end
