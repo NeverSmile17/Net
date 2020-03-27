8f2607cad067dd0ae58b10c08ca7ee3d42c4c4a0
NeverSmile17 <62741476+NeverSmile17@users.noreply.github.com>
2020-03-27 14:44:08 +0300

    Delete A_func.m

--- A_func.m
+++
@@ -1,9 +0,0 @@
-function A = A_func(codes)
-size_A = length(codes(1, :)) + 1;
-A = zeros(1, size_A);
-
-w = sum(codes, 2);
-
-for i = 1 : size_A
-    A(1, i) = sum(((i - 1) == w));    слов определенного веса 
-end
